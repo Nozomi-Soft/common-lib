5f9107b185211fcfeebdffaf98ef86b349da9d4b
Pedro Huerta <acmilanp7@hotmail.com>
2018-11-29 14:59:43 -0600

    Initial commit

---
+++ src/test/java/com/nozomisoftnet/CommonTest.java
@@ -0,0 +1,16 @@
+package com.nozomisoftnet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class CommonTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
